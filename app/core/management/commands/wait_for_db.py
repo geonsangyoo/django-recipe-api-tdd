3ffda072f94de67563b0ae165001d89f8854f506
Geonsang Yoo <geonsangyoo@gmail.com>
2022-02-20 15:38:16 +0900

    token test is added

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,14 +9,14 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write('waiting for database...')
+        # self.stdout.write('waiting for database...')
 
         db_conn = None
         while not db_conn:
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                # self.stdout.write('waiting 1 second...')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database is available!!'))
+        # self.stdout.write(self.style.SUCCESS('Database is available!!'))
